a3ec1e9cf749d85b148a8edb2b3fb7bfc6f3c525
dinushka114 <dinushkapiyumal678@gmail.com>
2021-09-22 21:08:33 +0530

    add pages controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',  [PagesController::class, 'index']);
+Route::get('/about',  [PagesController::class, 'about']);
+
+
+Route::get('/users/{id}', function ($id) {
+    return 'User with id' . $id;
 });
