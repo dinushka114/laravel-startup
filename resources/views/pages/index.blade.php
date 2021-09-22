c61f4a299fe74c5b3bb3a64677dc58028ea55b2f
dinushka114 <dinushkapiyumal678@gmail.com>
2021-09-22 21:13:15 +0530

    add layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app_name'  , 'MyApp') }}</title>
-</head>
-<body>
+@extends('layout.app')
 
-<h1>Home Page</h1>
+@section('content')
     
-</body>
-</html>
+    <h1>Welcome MyApp</h1>
+
+@endsection
